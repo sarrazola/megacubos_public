f753532c1a616d884114854096799bf0fcda2b60
Andrés Sarrazola <andres@sarrazola.com>
2024-11-30 00:23:49 -0500

    sizes of cards components merged on sizes preview and real

---
+++ src/utils/componentSizes.ts
@@ -0,0 +1,20 @@
+export const getDefaultSize = (type: string) => {
+    switch (type) {
+      case 'button':
+        return { width: 150, height: 95 };
+      case 'scorecard':
+        return { width: 200, height: 160 };
+      case 'table':
+        return { width: 700, height: 300 };
+      case 'text':
+        return { width: 240, height: 100 };
+      case 'chart':
+        return { width: 400, height: 300 };
+      case 'pdf':
+        return { width: 400, height: 300 };
+      case 'image':
+        return { width: 400, height: 300 };
+      default:
+        return { width: 400, height: 300 };
+    }
+  };
